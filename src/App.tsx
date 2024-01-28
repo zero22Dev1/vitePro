6d8972811f11195a7a0dba54adfc8c59ed33d77a
zero22Dev1 <zero22dev@gmail.com>
2024-01-28 19:46:03 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+
+      <div className="bg-black text-white"  >
+        <p>DD</p>
+        <Button>サンプル</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
